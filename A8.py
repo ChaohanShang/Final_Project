46947eb4d848b1563f31848d48e5bdc0aed6f55f
shang.chaohan <shang.chaohan@gmail.com>
2018-04-17 20:53:04 -0500

    fixed bug: def parking_timeslot() def select_checkzone()

--- A8.py
+++ A8.py
@@ -6,13 +6,16 @@ from itertools import combinations,permutations
 def parking_timeslot():
     # Time slot that need to park, x is the number of courses.
     x = np.random.random_integers(1,6)
-    return list(combinations(range(15),x))[np.random.random_integers(1,len(list(combinations(range(15),x))))]
+    possibleresult = list(combinations(range(15),x))
+    select = np.random.random_integers(0,len(possibleresult))
+    return possibleresult[select]
 
 def select_checkzone():
     # Divide the map into 20 parts, each time search 3 out of 20.
     # Uniformly pick 3 zones for checking.
-    checklist = list(combinations(range(20),3))[np.random.random_integers(1,len(list(combinations(range(20),3))))]
-    return checklist
+    possibleresult = list(combinations(range(20),3))
+    select = np.random.random_integers(0,len(possibleresult))
+    return possibleresult[select]
 
 def parking_check_schedule():
     # the parking enforcement check every hour from Monday through Saturday
@@ -38,8 +41,8 @@ for i in range(16):
         for j in range(0,9):
             check_schedule.append(select_checkzone())
     check_schedule = np.array(check_schedule).reshape(5,3,9)
-    # print(check_schedule)
-    # print("-----------------")
+    print(check_schedule)
+    print("-----------------")
     for i in range(len(courseinfo)):
         x = courseinfo[i][0]
         y = courseinfo[i][1]

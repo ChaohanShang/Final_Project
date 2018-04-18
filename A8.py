f9c539a942f6b18070d6aaa3dd9e60f4556643a9
shang.chaohan <shang.chaohan@gmail.com>
2018-04-17 22:04:41 -0500

    fixed bug: def parking_timeslot() def select_checkzone()

--- A8.py
+++ A8.py
@@ -7,14 +7,14 @@ def parking_timeslot():
     # Time slot that need to park, x is the number of courses.
     x = np.random.random_integers(1,6)
     possibleresult = list(combinations(range(15),x))
-    select = np.random.random_integers(0,len(possibleresult))
+    select = np.random.random_integers(0,len(possibleresult)-1)
     return possibleresult[select]
 
 def select_checkzone():
     # Divide the map into 20 parts, each time search 3 out of 20.
     # Uniformly pick 3 zones for checking.
     possibleresult = list(combinations(range(20),3))
-    select = np.random.random_integers(0,len(possibleresult))
+    select = np.random.random_integers(0,len(possibleresult)-1)
     return possibleresult[select]
 
 def parking_check_schedule():
@@ -26,6 +26,12 @@ def parking_check_schedule():
             check_schedule.append(select_checkzone())
     return check_schedule
 
+def meter():
+    return np.random.random_integers(0,1)
+
+def tow():
+    return np.random.random_integers(0,1)
+
 timeslot = parking_timeslot()
 courseinfo = []
 for i in range(len(timeslot)):
@@ -33,24 +39,36 @@ for i in range(len(timeslot)):
     courseinfo.append(t)
 print(courseinfo)
 
-caughttime = 0
-for i in range(16):
-    print("-----------------")
-    check_schedule = []
-    for i in range(0,5):
-        for j in range(0,9):
-            check_schedule.append(select_checkzone())
-    check_schedule = np.array(check_schedule).reshape(5,3,9)
-    print(check_schedule)
-    print("-----------------")
-    for i in range(len(courseinfo)):
-        x = courseinfo[i][0]
-        y = courseinfo[i][1]
-        print(check_schedule[x][y])
-        if 1 in check_schedule[x][y]:
-            print("dang")
-            caughttime +=1
-        else:
-            print("safe")
+# meter = True
+# tow = True
+ttltkt = 0
+ttltow = 0
+for i in range(100):
+    cost = 0
+    towtime = 0
+    tickettime = 0
+    for ii in range(32):
+        check_schedule = []
+        for i in range(0,5):
+            for j in range(0,9):
+                check_schedule.append(select_checkzone())
+        check_schedule = np.array(check_schedule).reshape(5,3,9)
+        print(check_schedule)
+        print("-----------------")
+        for i in range(len(courseinfo)):
+                x = courseinfo[i][0]
+                y = courseinfo[i][1]
+                print(check_schedule[x][y])
+                if meter() == 0:
+                    if tow() ==1:
+                        towtime += 1
+                    else:
+                        if 1 in set(check_schedule[x][y]):
+                            tickettime +=1
+                else:
+                    cost += 3
+    ttltkt += tickettime
+    ttltow += towtime
+print("tkt",ttltkt/100)
+print("tow",ttltow/100)
 
-print(caughttime)
